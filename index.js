614bb05d3b5a4fb44556670aceaf3f8a5b7a9e7e
andreip1123 <andrei_popa@sync.ro>
2018-03-09 17:34:25 +0200

    Added the makeXmlEntry function.

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
 module.exports = {
+  getLanguages: function () {
+    return /*todo: get it from xml OR */ ['en_US', 'de_DE', 'fr_FR', 'ja_JP', 'nl_NL'];
+  },
+
   doit: function (msg) {
     return 'did it ' + msg;
+  },
+
+  /**
+   * Generate an entry for translation_webapp.xml from the translation objects added in translation.js.
+   * All languages will have the english string, this is a new, untranslated tag.
+   *
+   * @param key The value for the key element.
+   * @param trObj The translation object.
+   * @returns {string} A string with the xml entry to be added to translation.xml
+   */
+  makeXmlEntry: function (key, trObj) {
+    var languages = this.getLanguages();
+    var xmlEntryString =
+      '    <key distribution="webauthor" value="' + key + '">\r\n' +
+      '        ' + (trObj.comment ? '<comment>' + trObj.comment + '</comment>\r\n' : '<comment/>\r\n');
+    languages.forEach(function(language) {
+      xmlEntryString +=
+        '        <val lang="' + language + '">' + ((trObj[language]) ? trObj[language] : trObj['en_US']) + '</val>\r\n'
+    });
+    xmlEntryString +=
+      '    </key>\r\n';
+    return xmlEntryString;
   }
-}
+};
--- test/index.js
+++ test/index.js
@@ -1,9 +1,36 @@
 var should = require('chai').should();
 var synci18n = require('../index');
-var doit = synci18n.doit;
 
 describe('doitproperly', function () {
   it('appends to string', function () {
-    doit('testmessage').should.equal('did it testmessage');
+    synci18n.doit('testmessage').should.equal('did it testmessage');
+  })
+});
+
+describe('makeXmlEntry', function () {
+  it('should make new xml entry', function () {
+    var languages = synci18n.getLanguages();
+    var tag = 'NEW_TAG_';
+    var englishMessage = 'This is a new tag';
+    var comment = 'Trying to add new tag';
+
+
+    var xmlEntry = synci18n.makeXmlEntry('NEW_TAG_', { comment: comment, en_US: englishMessage });
+    var allChecksPassed = true;
+
+    languages.forEach(function (languageCode) {
+      if (xmlEntry.indexOf(languageCode) === -1) {
+        allChecksPassed = false;
+      }
+    });
+
+    if (xmlEntry.indexOf(comment) === -1 ||
+      xmlEntry.indexOf(tag) === -1 ||
+      xmlEntry.split(englishMessage).length - 1 !== languages.length||
+      xmlEntry.indexOf('distribution="webauthor"') === -1) {
+      allChecksPassed = false;
+    }
+
+    should.equal(allChecksPassed, true);
   })
 });
