9f8a11d6c8fd6e3f76977aaea9cf97dc1962341f
andreip1123 <andrei_popa@sync.ro>
2018-03-13 15:13:13 +0200

    Use xml2js, add getLanguages, makeTranslationJsons.

--- index.js
+++ index.js
@@ -1,31 +1,70 @@
-module.exports = {
-  getLanguages: function () {
-    return /*todo: get it from xml OR */ ['en_US', 'de_DE', 'fr_FR', 'ja_JP', 'nl_NL'];
-  },
+const fs = require('fs');
+const path = require('path');
 
-  doit: function (msg) {
-    return 'did it ' + msg;
-  },
+var xml2js = require('xml2js');
 
-  /**
-   * Generate an entry for translation_webapp.xml from the translation objects added in translation.js.
-   * All languages will have the english string, this is a new, untranslated tag.
-   *
-   * @param key The value for the key element.
-   * @param trObj The translation object.
-   * @returns {string} A string with the xml entry to be added to translation.xml
-   */
-  makeXmlEntry: function (key, trObj) {
-    var languages = this.getLanguages();
-    var xmlEntryString =
-      '    <key distribution="webauthor" value="' + key + '">\r\n' +
-      '        ' + (trObj.comment ? '<comment>' + trObj.comment + '</comment>\r\n' : '<comment/>\r\n');
-    languages.forEach(function(language) {
-      xmlEntryString +=
-        '        <val lang="' + language + '">' + ((trObj[language]) ? trObj[language] : trObj['en_US']) + '</val>\r\n'
-    });
-    xmlEntryString +=
-      '    </key>\r\n';
-    return xmlEntryString;
+function Synci18n(options) {
+
+  options = options || {};
+  this.sourceFile = options.sourceFile;
+  this.destinationFolder = options.destinationFolder;
+}
+
+Synci18n.prototype.getLanguages = function () {
+  var languages;
+  var sourceFile = this.sourceFile;
+  if (sourceFile) {
+    console.log('getting languages from file');
+    languages = JSON.parse(fs.readFileSync(sourceFile, 'utf8'));
+    languages = languages.translation.languageList[0].language;
+
+    var languageCodes = [];
+    languages.forEach(language => languageCodes.push(language['$'].lang));
+    return languageCodes;
+  } else {
+    console.log('getting default languages');
+    return ['en_US', 'de_DE', 'fr_FR', 'ja_JP', 'nl_NL'];
   }
 };
+
+// get the XML files and transform them into JSON
+/*"translation-xml-to-json"*/
+Synci18n.prototype.makeTranslationJsons = function () {
+  var sourceFile = this.sourceFile;
+  var destinationFolder = this.destinationFolder;
+
+  var parser = new xml2js.Parser();
+  var xmlFileContent = fs.readFileSync(sourceFile, 'utf8');
+
+  parser.parseString(xmlFileContent, (err, result) => {
+    if (destinationFolder) {
+      fs.writeFileSync(destinationFolder + '/translation.json', JSON.stringify(result), 'utf8');
+    } else {
+      console.error('could not find temp folder');
+    }
+  });
+};
+
+/**
+ * Generate an entry for translation_webapp.xml from the translation objects added in translation.js.
+ * All languages will have the english string, this is a new, untranslated tag.
+ *
+ * @param key The value for the key element.
+ * @param trObj The translation object.
+ * @returns {string} A string with the xml entry to be added to translation.xml
+ */
+Synci18n.prototype.makeXmlEntry = function (key, trObj) {
+  var languages = this.getLanguages();
+  var xmlEntryString =
+    '    <key distribution="webauthor" value="' + key + '">\r\n' +
+    '        ' + (trObj.comment ? '<comment>' + trObj.comment + '</comment>\r\n' : '<comment/>\r\n');
+  languages.forEach(function(language) {
+    xmlEntryString +=
+      '        <val lang="' + language + '">' + ((trObj[language]) ? trObj[language] : trObj['en_US']) + '</val>\r\n'
+  });
+  xmlEntryString +=
+    '    </key>\r\n';
+  return xmlEntryString;
+};
+
+module.exports = Synci18n;
--- test/index.js
+++ test/index.js
@@ -1,20 +1,21 @@
 var should = require('chai').should();
-var synci18n = require('../index');
-
-describe('doitproperly', function () {
-  it('appends to string', function () {
-    synci18n.doit('testmessage').should.equal('did it testmessage');
-  })
-});
+var Synci18n = require('../index');
+var fs = require('fs');
+var xml2js = require('xml2js');
+const path = require('path');
 
 describe('makeXmlEntry', function () {
   it('should make new xml entry', function () {
-    var languages = synci18n.getLanguages();
+    var sourceFile = __dirname + '/temp/translation.json';
+    console.log('sourcefile');
+
     var tag = 'NEW_TAG_';
     var englishMessage = 'This is a new tag';
     var comment = 'Trying to add new tag';
 
 
+    var synci18n = new Synci18n();
+    var languages = synci18n.getLanguages();
     var xmlEntry = synci18n.makeXmlEntry('NEW_TAG_', { comment: comment, en_US: englishMessage });
     var allChecksPassed = true;
 
@@ -33,4 +34,36 @@ describe('makeXmlEntry', function () {
 
     should.equal(allChecksPassed, true);
   })
+});
+
+describe('makeTranslationJsons', function () {
+  it('json from xml should be created', function () {
+    var sourceFile = './test/translation.xml';
+    var destinationFolder = __dirname + '/temp';
+
+    var synci18n = new Synci18n({
+      sourceFile,
+      destinationFolder
+    });
+
+    var expectedOutputFile = destinationFolder + '/' + path.basename(sourceFile, path.extname(sourceFile)) + '.json';
+
+    if (fs.existsSync(expectedOutputFile)) {
+      fs.unlinkSync(expectedOutputFile);
+    }
+
+    synci18n.makeTranslationJsons();
+    fs.existsSync(expectedOutputFile).should.equal(true);
+  });
+});
+
+describe('getLanguages', function () {
+  it('should get the languages provided by the file', function () {
+    var sourceFile = __dirname + '\\temp\\translation.json';
+    var synci18n = new Synci18n({
+      sourceFile
+    });
+    var languages = synci18n.getLanguages(sourceFile);
+    languages.should.eql(languages, ['en_US', 'de_DE', 'nl_NL', 'fr_FR', 'ja_JP']);
+  });
 });
